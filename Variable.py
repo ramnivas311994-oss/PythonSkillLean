7648fba36883335dd7e2145d8f9b2bab8d262f7d
Ramnivas <ramnivas311994@gmail.com>
2026-04-04 00:47:01 -0300

    Variable

---
+++ Variable.py
@@ -0,0 +1,16 @@
+#variables #
+name = "Ramnivas"
+age = 32
+job = "Software Engineer"
+city = "Canada Halifax"
+salary = 101.10
+likes_coding = True
+current_year = 2026
+
+print(name)
+print(age)
+print(job)
+print(salary)
+print(likes_coding)
+print(current_year)
+
